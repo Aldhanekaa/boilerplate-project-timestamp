43d82f82bb57036c598082bbe18eb6457fdeaa94
Aldhaneka <aldhanekadev@gmail.com>
2020-11-17 13:42:00 +0000

    Initial Commit From Aldhaneka

--- server.js
+++ server.js
@@ -14,14 +14,65 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
+
+const convertDateToUnix = input => {
+  return input !== undefined ? new Date(input).getTime() : new Date().getTime();
+}
+
+const convertUnixToDate = Unix => {
+  var x = new Date(Unix);
+  const data = {
+    months: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    days:["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  }
+  var year = x.getFullYear();
+  var month = data.months[x.getMonth()];
+  var day = data.days[x.getDay()]
+  var date = x.getDate();
+  var hour = x.getHours();
+  var min = x.getMinutes();
+  var sec = x.getDay();
+  console.log(x.getDay())
+  const time = `${day}, ${date} ${month} ${year}`
+  return time
+}
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/timestamp", function(req,res) {
+  var unix = new Date().getTime();
+  var utc = convertUnixToDate(unix)
+  res.json({
+    unix,
+    utc 
+  })
+})
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:input", function (req, res) {
+  let {input} = req.params;
+  let checkWord = /[a-z]/ig
+  input.toString();
+  if (input.includes("-") && !checkWord.test(input)) {
+    console.log('tess')
+    input = input.split("-");
+    var arr = [];
+
+    arr[0] = input[0] || "1970";
+    arr[1] = input[1] || "1";
+    arr[2] = input[2] || "1"
+    console.log(arr.join('-'))
+    let unix = convertDateToUnix(arr.join("-"));
+    let date = convertUnixToDate(unix);
+    res.json({unix, utc: date})
+  }else if (!input.includes("-") && !checkWord.test(input)) {
+    let utc = convertUnixToDate(parseInt(input));
+    res.json({unix:input, utc})
+  }else {
+    res.json({error: 'Invalid Date'});
+  }
 });
 
 
